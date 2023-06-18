61e9a694f652d1032c7020aa21fe4ce6fa7c3718
kccahuih <kccahuih@emeal.nttdata.com>
2023-06-18 17:53:38 -0500

    Archivo properties para el perfil de despliegue

--- src/test/java/com/reto/cliente/repository/RepositorioClienteTest.java
+++ src/test/java/com/reto/cliente/repository/RepositorioClienteTest.java
@@ -7,12 +7,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@ActiveProfiles("test")
 class RepositorioClienteTest {
 
     @Autowired
